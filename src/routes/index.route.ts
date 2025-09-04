5f68eb535942b2c3562bf8e8830397981f8c6438
getparas <karkiparas020@gmail.com>
2025-09-04 19:20:10 +0545

    add some stoker openapi helpers

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,27 +1,25 @@
 import { createRoute, z } from "@hono/zod-openapi";
+import * as HttpStatusCode from "stoker/http-status-codes";
+import { jsonContent } from "stoker/openapi/helpers";
+import { createMessageObjectSchema } from "stoker/openapi/schemas";
 
 import { createRouter } from "@/lib/create-app.js";
 
 const router = createRouter()
   .openapi(createRoute({
+    tags: ["Index"],
     method: "get",
     path: "/",
     responses: {
-      200: {
-        content: {
-          "application/json": {
-            schema: z.object({
-              message: z.string(),
-            }),
-          },
-        },
-        description: "Task API Index",
-      },
+      [HttpStatusCode.OK]: jsonContent(
+        createMessageObjectSchema("Tasks API"),
+        "Task API Index",
+      ),
     },
   }), (c) => {
     return c.json({
       message: "Tasks API",
-    });
+    }, HttpStatusCode.OK);
   });
 
 export default router;
